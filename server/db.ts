a0421fe51231b474170a242d2ad90417ed3bbecc
Sujal <sujal@example.com>
2025-12-29 23:42:30 +0530

    thirdsss

--- server/db.ts
+++ server/db.ts
@@ -10,5 +10,10 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // <-- This disables strict SSL verification (self-signed cert workaround)
+  },
+});
 export const db = drizzle(pool, { schema });
