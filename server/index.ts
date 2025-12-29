a4b063db328d394783ccfce65915f3445726be7c
Sujal <sujal@example.com>
2025-12-29 23:04:30 +0530

    second

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
+import "dotenv/config";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
@@ -84,15 +85,10 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+const port = parseInt(process.env.PORT || "5000", 10);
+
+httpServer.listen(port, () => {
+  log(`serving on port ${port}`);
+});
+
 })();
